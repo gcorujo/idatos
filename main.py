fc613e24525c39599a7ccdefa5364ab1e471c137
gcorujo <corujo.gabriel@gmail.com>
2022-11-19 13:59:06 -0300

    Se obtiene la informacin del dataset de IMDB y del web scrapper de Rotten Tomatoes

--- main.py
+++ main.py
@@ -1,39 +1,87 @@
 import json
+import csv
+import difflib
 
 from flask import Flask, request
 from flask_restful import Resource, Api
 
-from PyMovieDb import IMDB
 from rotten_tomatoes_scraper.rt_scraper import MovieScraper
+from rotten_tomatoes_scraper.rt_scraper import DirectorScraper
 
 app = Flask(__name__)
 api = Api(app)
-
-imdb = IMDB()
 
 class Pelicula(Resource):
     #Se agregan los metodos aca
     def get(self):
         query = request.args.get('buscar')
         
-        #Se obtiene la informacion de la pelicula desde IMDB
-        infoPeliculasIMDB = imdb.search(query)
-        #Se convierte el string json para poder manejarlo como un diccionario
-        infoPeliculasIMDB  = json.loads(infoPeliculasIMDB)
-        
-        #if (infoPeliculasIMDB['result_count'] > 0):
-            #listaPeliculasIMDB = infoPeliculasIMDB['results']
+        moviesTitleIMDB = []
+        indice = 0
+        with open('imdb_top_1000.csv','rt') as dataset:
+            data = csv.reader(dataset)
+            for movie in data:
+                moviesTitleIMDB.append(movie[1])
+        dataset.close() 
 
-        #pelicula = listaPeliculasIMDB[1] No funciona asi
-        #print("test" + str(pelicula), flush=True) 
-            
-
-        #Se obtiene la informacion de la pelicula desde Rotten Tomatoes
-        movie_scraper = MovieScraper(movie_title=query)
-        movie_scraper.extract_metadata()
-        peliculaRT = movie_scraper.metadata
+        titlesMap = {}
+        for i,nombre in enumerate(moviesTitleIMDB):
+            titlesMap[nombre] = i 
         
-        return {'data': {'peliculaIMDB': infoPeliculasIMDB, 'peliculaRT': peliculaRT}}, 200 
+        #Se busca los nombres de las peliculas que matchean mejor con la query
+        titlesMatchesIMDB = difflib.get_close_matches(query,titlesMap.keys())
+        titleIMDB = titlesMatchesIMDB[0]
+        indicePeliculaElegida = titlesMap[titleIMDB]
+        print(indicePeliculaElegida)
+
+        with open('imdb_top_1000.csv','rt') as dataset:
+            data = csv.reader(dataset)
+            for i, row in enumerate(data):
+                if i == indicePeliculaElegida:
+                    infoPelicula = row 
+                    break
+        dataset.close()
+        
+        #Info pelicula del dataset de IMDB
+        yearIMDB = infoPelicula[2]
+        genresIMDB = infoPelicula[5]
+        scoreIMDB = infoPelicula[6]
+        directorIMDB = infoPelicula[9]
+
+        #Se busca en el web scrapper de RT todas las peliculas que hizo el director
+        director_scraper = DirectorScraper(director_name=directorIMDB)
+        director_scraper.extract_metadata()
+        moviesTitleRT = list(director_scraper.metadata.keys())
+        moviesInfoRT = director_scraper.metadata
+        
+        #Se matchea la lista de peliculas del director con el nombre de la pelicula del dataset de IMDB
+        titlesMatchesRT = difflib.get_close_matches(titleIMDB, moviesTitleRT)
+        #titleMovieRT = titlesMatchesRT[0] 
+       
+        movieTitleRT = ''
+        for titleMovieRT in titlesMatchesRT:
+            #Se obtiene la informacion de la pelicula en RT
+            yearRT = moviesInfoRT[titleMovieRT]['Year']
+            scoreRT = moviesInfoRT[titleMovieRT]['Score_Rotten'] 
+        
+            if yearRT == yearIMDB:
+                movieTitleRT = titleMovieRT
+                break
+        
+        movie_scraper = MovieScraper(movie_title=movieTitleRT)
+        movie_scraper.extract_metadata()     
+       
+        scoreRT = movie_scraper.metadata['Score_Rotten']
+        scoreAudienceRT = movie_scraper.metadata['Score_Audience']
+        genresRT = movie_scraper.metadata['Genre']
+       
+        #Integracion de Generos 
+        for genre in genresRT:
+            if (genre == 'Mystery&thriller'):
+                print('Encontro el genero')
+
+
+        return {'data': {'IMDB': { 'titleIMDB': titleIMDB, 'scoreIMDB': scoreIMDB, 'directorIMDB': directorIMDB, 'yearIMDB': yearIMDB, 'genresIMDB': genresIMDB}, 'RottenTomates': {'titleRT': titleMovieRT, 'scoreRT': scoreRT, 'scoreAudienceRT': scoreAudienceRT, 'genresRT': genresRT} } }, 200 
 
 api.add_resource(Pelicula,'/pelicula')
 
